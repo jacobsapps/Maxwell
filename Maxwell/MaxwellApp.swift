c86ab843bdc8bae0346e60c2af111440bb9659b6
Jacob Bartlett <>
2026-01-20 14:15:41 +0000

    Initial commit

---
+++ Maxwell/MaxwellApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxwellApp.swift
+//  Maxwell
+//
+//  Created by Jacob Bartlett on 20/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MaxwellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MaxwellTests/MaxwellTests.swift
@@ -0,0 +1,17 @@
+//
+//  MaxwellTests.swift
+//  MaxwellTests
+//
+//  Created by Jacob Bartlett on 20/01/2026.
+//
+
+import Testing
+@testable import Maxwell
+
+struct MaxwellTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
